90b348affbf51fb70c0b954e3f1ce36f8e9e1ee5
Karthik371 <70939080+Karthik371@users.noreply.github.com>
2021-05-11 10:24:54 +0530

    new

--- mongoose/connection.js
+++ mongoose/connection.js
@@ -3,11 +3,14 @@
 const monoose = require("mongoose");
 
 const db = () => {
-  monoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  });
+  monoose.connect(
+    " mongodb+srv://Admin-Karthik:qwerty12@cluster0.semtp.mongodb.net/Musicplayer",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  );
 };
 module.exports = db;
